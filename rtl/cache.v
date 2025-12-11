a2b9188aeb15367da30e08d540ed8c4af8e864b3
eperez <ejperez3@wisc.edu>
2025-12-10 21:14:05 -0600

    7 errors

--- rtl/cache.v
+++ rtl/cache.v
@@ -196,15 +196,16 @@ module cache (
 
   //cycle to include word wanted and the following three words
   assign o_req_addr_offset = i_req_addr + {28'b0, mem_add_read,2'b0};
-  assign o_mem_addr=(state==MEMREAD)? o_req_addr_offset:
-    (state==MEMWRITE)? i_req_addr:
-    32'b0;
+
+  assign o_mem_addr = (state==MEMREAD)? o_req_addr_offset:
+                      (state==MEMWRITE)? i_req_addr:
+                       32'b0;
   assign o_mem_ren=(state==MEMREAD)?1'b1:1'b0;
   
   //logic for loading 4 words of data on any read from memory
   reg[1:0] block_offset;
   always @(posedge i_clk) begin
-    if (i_rst) begin
+    if (i_rst || state == IDLE) begin
       block_offset<=2'b0;
       mem_add_read <= 2'b0;
     end
